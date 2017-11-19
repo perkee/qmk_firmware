bccff511a214c44aab71e9541404ac31b5440a58
perkee <hello@perk.ee>
2017-11-19 00:47:51 -0500

    rebased removing send_string so nuked meme macros

--- keyboards/lets_split/keymaps/perkee/keymap.c
+++ keyboards/lets_split/keymaps/perkee/keymap.c
@@ -45,9 +45,6 @@ enum custom_keycodes {
 #define KC_NUMD LT(_NUMPAD, KC_D)
 #define KC_SYMS LT(_SYMBOL, KC_S)
 
-#define KC_M0 M(0)
-#define KC_M1 M(1)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = KC_KEYMAP(
@@ -80,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────┼────┼────┼────┼────┼────┤    ├────┼────┼────┼────┼────┼────┤
          ,    ,    ,    ,    ,    ,         , 4  , 5  , 6  ,MINS,    ,
   //├────┼────┼────┼────┼────┼────┤    ├────┼────┼────┼────┼────┼────┤
-         ,    ,    ,    ,    ,    ,         , 1  , 2  , 3  ,ASTR, M0 ,
+         ,    ,    ,    ,    ,    ,         , 1  , 2  , 3  ,ASTR,    ,
   //├────┼────┼────┼────┼────┼────┤    ├────┼────┼────┼────┼────┼────┤
-         ,    ,    ,    ,    ,    ,     MINS, 0  ,DOT ,ENT ,SLSH, M1
+         ,    ,    ,    ,    ,    ,     MINS, 0  ,DOT ,ENT ,SLSH,
   //└────┴────┴────┴────┴────┴────┘    └────┴────┴────┴────┴────┴────┘
   ),
 
@@ -214,40 +211,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  if (record->event.pressed) {
-    switch(id) {
-      case 0:
-        SEND_STRING("argument from popularity much");
-        return false;
-      case 1:
-        SEND_STRING(
-          "What the fuck did you just fucking say about me, you little bitch?"
-          " I'll have you know I graduated top of my class in the Navy Seals, and"
-          " I've been involved in secret raids on Al-Quaeda, and I have over 300"
-          " confirmed kills. I am trained in gorilla warfare and I'm the top sniper"
-          " in the entire US armed forces. You are nothing to me but just another"
-          " target. I will wipe you out with precision the likes of which has"
-          " never been seen before on this Earth, mark my words. You think you"
-          " can get away with saying shit to me over the Internet? Think again,"
-          " fucker. As we speak I am contacting my network of spies across the"
-          " USA and your IP is being traced right now so you better prepare for"
-          " the storm, maggot. The storm that wipes out the pathetic little thing"
-          " you call your life. You're fucking dead, kid. I can be anywhere,"
-          " anytime, and I can kill you in over seven hundred ways, and that's"
-          " just with my bare hands. Not only am I extensively trained in unarmed"
-          " combat, but I have access to the entire arsenal of the United States"
-          " Marine Corps and I will use it to its full extent to wipe your ass"
-          " off the face of the continent, you little shit. If only you could"
-          " have known what unholy retribution your little \"clever\" comment was"
-          " about to bring down upon you, maybe you would have held your tongue."
-          " You didn't, and now you're paying the price, you goddamn idiot."
-          " I will shit all over you and you will drown in it."
-          " You're fucking dead, kiddo."
-        );
-        return false;
-    }
-  }
-  return MACRO_NONE;
-};
